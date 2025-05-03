a762427e4826ed14e4468bf0663d2cb4ed77dbcd
Mauricio <maugarciavar@unal.edu.co>
2025-05-03 11:32:54 -0500

    homework2copy5

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -9,23 +9,13 @@ from ._internals.write_count_words import write_count_words
 
 def main():
 
-    
-    all_lines = read_all_lines()
+    input_folder = "data/input"
+    output_folder = "data/output"
+    all_lines = read_all_lines(input_folder)
     all_lines = preprocessing_lines(all_lines)
     words = split_in_words(all_lines)
     counter = count_words(words)
-
-    # count the frequency of the words in the files in the input directory
-    # counter = {}
-    # for filename in input_file_list:
-    #     with open("data/input/" + filename) as f:
-    #         for l in f:
-    #             for w in l.split():
-    #                 w = w.lower().strip(",.!?")
-    #                 counter[w] = counter.get(w, 0) + 1
-
-    ##
-    write_count_words(counter)
+    write_count_words(counter, output_folder)
 
 if __name__ == "__main__":
     main()
